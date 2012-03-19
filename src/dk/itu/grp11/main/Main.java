be093533d6891107497aa5a66fb537a88c26d9f7
Anders <hojmark.dk@gmail.com>
2012-03-19 13:11:13 +0100

    Another one

--- src/dk/itu/grp11/main/Main.java
+++ src/dk/itu/grp11/main/Main.java
@@ -2,6 +2,6 @@ package dk.itu.grp11.main;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Program compiles as well :D :D :D");
+    System.out.println("Program compiles as well :D :D :D :D");
   }
 }
