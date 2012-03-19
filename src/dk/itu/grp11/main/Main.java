ab659abb91fc23d9993a009e20f29df46d02aba0
Lauge Djuraas <lauge.djuraas@hotmail.com>
2012-03-19 14:40:39 +0100

    Added some (empty) classes

--- src/dk/itu/grp11/main/Main.java
+++ src/dk/itu/grp11/main/Main.java
@@ -3,5 +3,7 @@ package dk.itu.grp11.main;
 public class Main {
   public static void main(String[] args) {
     System.out.println("Program compiles as well :D :D :D :D");
+    
+    new Parser();
   }
 }
---
+++ src/dk/itu/grp11/main/Parser.java
@@ -0,0 +1,5 @@
+package dk.itu.grp11.main;
+
+public class Parser {
+
+}
