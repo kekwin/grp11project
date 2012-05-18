1061f3e026aab5797772059043f624f7c0385df4
Anders <hojmark.dk@gmail.com>
2012-05-18 15:16:37 +0200

    Updated TrafficDirection TEST. Possibly finished.

--- src/dk/itu/grp11/test/TrafficDirectionTest.java
+++ src/dk/itu/grp11/test/TrafficDirectionTest.java
@@ -8,13 +8,39 @@ import dk.itu.grp11.enums.TrafficDirection;
 
 public class TrafficDirectionTest {
 
+  //Proper input
   @Test
-  public void test() {
-    fail("Not yet implemented");
-    System.out.println(TrafficDirection.getDirectionById("tf"));
-    System.out.println(TrafficDirection.getDirectionById("ft"));
-    System.out.println(TrafficDirection.getDirectionById(""));
-    System.out.println(TrafficDirection.getDirectionById("n"));
+  public void convertCleanInputTest() {
+    assertEquals(TrafficDirection.BOTH_WAYS, TrafficDirection.getDirectionById(""));
+    assertEquals(TrafficDirection.DRIVING_NOT_ALLOWED, TrafficDirection.getDirectionById("n"));
+    assertEquals(TrafficDirection.FROM_TO, TrafficDirection.getDirectionById("ft"));
+    assertEquals(TrafficDirection.TO_FROM, TrafficDirection.getDirectionById("tf"));
   }
-
+  
+  //Different bad inputs (some which are handled in the getDirectionById()-method)
+  @Test
+  public void convertBadInputTest() {
+    boolean b;
+    
+    if(TrafficDirection.BOTH_WAYS == TrafficDirection.getDirectionById("b")) b = true; else b = false;
+    assertFalse(b);
+    
+    if(TrafficDirection.BOTH_WAYS == TrafficDirection.getDirectionById(" ")) b = true; else b = false;
+    assertTrue(b);
+    
+    if(TrafficDirection.BOTH_WAYS == TrafficDirection.getDirectionById(",")) b = true; else b = false;
+    assertFalse(b);
+    
+    if(TrafficDirection.FROM_TO == TrafficDirection.getDirectionById("ft ")) b = true; else b = false;
+    assertTrue(b);
+    
+    if(TrafficDirection.FROM_TO == TrafficDirection.getDirectionById(" ft")) b = true; else b = false;
+    assertTrue(b);
+    
+    if(TrafficDirection.FROM_TO == TrafficDirection.getDirectionById(" ft ")) b = true; else b = false;
+    assertTrue(b);
+    
+    if(TrafficDirection.TO_FROM == TrafficDirection.getDirectionById("tff")) b = true; else b = false;
+    assertFalse(b);
+  }
 }
